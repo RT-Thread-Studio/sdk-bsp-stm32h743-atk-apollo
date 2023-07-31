7ddeb850d50daa44892182dff121ae90bf2cd7e0
yangjie11 <yangjiexaut@126.com>
2023-07-31 15:12:08 +0800

    update rtt to 4.1.1

--- default_project_0/rtconfig_preinc.h
+++ default_project_0/rtconfig_preinc.h
@@ -6,7 +6,11 @@
 /* RT-Thread pre-include file */
 
 #define HAVE_CCONFIG_H
+#define RT_USING_LIBC
+#define RT_USING_NEWLIB
 #define STM32H743xx
 #define USE_HAL_DRIVER
+#define _POSIX_C_SOURCE 1
+#define __RTTHREAD__
 
 #endif /*RTCONFIG_PREINC_H__*/
